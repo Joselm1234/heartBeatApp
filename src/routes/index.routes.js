cb3f9a0a4819819600290a838fc54a8757d69cc5
York <yorklouiscl@gmail.com>
2019-11-27 22:32:52 -0400

    working in login

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,6 +1,4 @@
-
-
-const admin = require('firebase-admin')
+const admin = require('firebase-admin');
 
 var serviceAccount = require("../../daBConnection/heartbeatapp7-firebase-adminsdk-gykhk-c5e64a1d2b.json");
 admin.initializeApp({
@@ -10,55 +8,73 @@ admin.initializeApp({
 });
 
 
-const {Router} = require('express');
+const { Router } = require('express');
 const router = Router();
 
 const db = admin.database();
+var user = admin.auth().currentUser;
+
 // pagina de inicio
-router.get('/',(req,res)=>{
-    db.ref('Usuario').once('value',(snapshot)=>{
-        const usuarios = snapshot.numChildren();
-        res.render('index', {cantidadDeUsuarios:usuarios});
-    })
-})
+router.get('/', (req, res) => {
+
+    if (user) {
+        db.ref('Usuario').once('value', (snapshot) => {
+            const usuarios = snapshot.numChildren();
+            res.render('index', { cantidadDeUsuarios: usuarios });
+        });
+    } else {
+        res.render('auth/login');
+    }
+});
+
+//ruta para el login
+router.get('/login', (req, res) => {
+    if (!user) {
+        res.render('auth/login');
+    } else {
+        res.render('/');
+    }
+});
 
 // ruta para registrar usuarios
-router.get('/login',(req,res)=>{
-    res.render('auth/login');
-})
-//ruta para el login
-router.get('/registrar',(req,res)=>{
+router.get('/registrar', (req, res) => {
     res.render('auth/registrar');
-})
+});
 
 // ruta para mostrar lista de usuarios
-router.get('/listarUsuarios', (req,res)=>{
-    db.ref('Usuario').once('value',(snapshot)=>{
-            const data = snapshot.val()
-            res.render('userLists', {usuarios:data})
-    })
-})
+router.get('/listarUsuarios', (req, res) => {
+    db.ref('Usuario').once('value', (snapshot) => {
+        const data = snapshot.val()
+        res.render('userLists', { usuarios: data })
+    });
+});
 
-router.post('/registrarUsuario', (req,res)=>{
+router.post('/registrarUsuario', (req, res) => {
     const newUser = {
         nombre: req.body.nombre,
         apellido: req.body.apellido,
         telefono: req.body.telefono,
         correo: req.body.correo,
         password: req.body.password
-    }
+    };
 
     //console.log(db.ref('usuariosWeb').isEqual(newUser).nombre);
     console.log(newUser);
-    
-    db.ref('usersWeb').push(newUser);
-    res.render('index')
-})
 
-/**
-router.get('/dashboard',(req,res)=>{
-    res.render('dashboard');
-})
-*/
+    db.ref('usersWeb').push(newUser);
+    res.render('index');
+});
+
+router.post('/auth', (req, res) => {
+
+    var ref = db.ref();
+    var userRef = ref.child('usersWeb');
+    console.log(userRef.toJSON());
+    userRef.orderByChild('correo').equalTo(req.body.email).on('child_added', function(snapshot) {
+        console.log(snapshot.val());
+    });
+
+});
+
 
 module.exports = router;
